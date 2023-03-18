df9384dae35b9097dda63d12f22bbe9d63ecc4f5
yezhen <chyezh@outlook.com>
2023-03-18 14:39:37 +0800

    add raii-style spin lock

--- sync/src/spin/mod.rs
+++ sync/src/spin/mod.rs
@@ -1,7 +1,9 @@
 use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Release};
 
+/// A raw spin lock implementation
 pub struct SpinLock<T> {
     locked: AtomicBool,
     value: UnsafeCell<T>,
@@ -21,17 +23,71 @@ impl<T> SpinLock<T> {
     }
 
     /// Acquire the spin lock and access the unique mutable reference of inner T
-    #[allow(clippy::mut_from_ref)]
-    pub fn lock(&self) -> &mut T {
+    pub fn lock(&self) -> SpinLockGuard<T> {
         // Must use acquire-release memory order to sync in multithread.
         while self.locked.swap(true, Acquire) {
             // Enter a spin loop
             std::hint::spin_loop();
         }
-        unsafe { &mut *self.value.get() }
+        SpinLockGuard { lock: self }
     }
 
-    pub fn unlock(&self) {
+    #[inline]
+    fn unlock(&self) {
         self.locked.store(false, Release);
+    }
+}
+
+/// A guard type acquired by SpinLock lock method
+pub struct SpinLockGuard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for SpinLockGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // Safety: There's only one guard of same value existed at a time,
+        // it's safe to access the inner value by any shared reference.
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for SpinLockGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // Safety: There's only one guard of same value existed at a time,
+        // it's safe to access the inner value with mutable reference by mutable reference of guard
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for SpinLockGuard<'_, T> {
+    fn drop(&mut self) {
+        // Unlock the corresponding spin lock when guard is dropped
+        self.lock.unlock();
+    }
+}
+
+mod tests {
+    #[allow(unused_imports)]
+    use super::SpinLock;
+    #[allow(unused_imports)]
+    use std::thread;
+
+    #[test]
+    fn test_spin_lock() {
+        for _ in 1..1000 {
+            let x = SpinLock::new(Vec::new());
+            thread::scope(|s| {
+                s.spawn(|| x.lock().push(1));
+                s.spawn(|| {
+                    let mut g = x.lock();
+                    g.push(2);
+                    g.push(2);
+                });
+            });
+            let g = x.lock();
+            assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+        }
     }
 }
