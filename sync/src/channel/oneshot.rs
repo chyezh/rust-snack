7d65652039e4c8e815708fa6ecfd71a5d73051a2
yezhen <chyezh@outlook.com>
2023-03-18 16:42:23 +0800

    unsafe oneshot channel implement

---
+++ sync/src/channel/oneshot.rs
@@ -0,0 +1,31 @@
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicBool};
+
+pub struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release);
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Acquire)
+    }
+
+    pub unsafe fn receive(&self) -> T {
+        (*self.message.get()).assume_init_read()
+    }
+}
