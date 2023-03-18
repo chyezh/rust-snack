681708f52d83d6ff1efa9cac9acc5af851fb711e
yezhen <chyezh@outlook.com>
2023-03-18 14:24:55 +0800

    add value protected spin lock

--- sync/src/spin/mod.rs
+++ sync/src/spin/mod.rs
@@ -1,23 +1,34 @@
+use std::cell::UnsafeCell;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Release};
 
-pub struct SpinLock {
+pub struct SpinLock<T> {
     locked: AtomicBool,
+    value: UnsafeCell<T>,
 }
 
-impl SpinLock {
-    pub const fn new() -> Self {
+/// Implement Sync if and only if T is Send
+/// Only one thread at a time access the T protected by reference,
+/// so T is not required to be Sync
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
         }
     }
 
-    pub fn lock(&self) {
+    /// Acquire the spin lock and access the unique mutable reference of inner T
+    #[allow(clippy::mut_from_ref)]
+    pub fn lock(&self) -> &mut T {
         // Must use acquire-release memory order to sync in multithread.
         while self.locked.swap(true, Acquire) {
             // Enter a spin loop
             std::hint::spin_loop();
         }
+        unsafe { &mut *self.value.get() }
     }
 
     pub fn unlock(&self) {
