101342c585fbc0172767b0c6cc9c3363ff435471
chyezh <chyezh@outlook.com>
2023-03-27 17:49:11 +0800

    optimizing unnessasary syscall in Condvar

--- sync/src/condvar/mod.rs
+++ sync/src/condvar/mod.rs
@@ -1,11 +1,12 @@
 use super::mutex::MutexGuard;
-use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::sync::atomic::{AtomicU32, AtomicUsize};
 
-use atomic_wait::{wait, wake_one};
+use atomic_wait::{wait, wake_all, wake_one};
 
 pub struct Condvar {
     counter: AtomicU32,
+    num_waiters: AtomicUsize,
 }
 
 impl Condvar {
@@ -13,23 +14,30 @@ impl Condvar {
     pub const fn new() -> Self {
         Self {
             counter: AtomicU32::new(0),
+            num_waiters: AtomicUsize::new(0),
         }
     }
 
     /// Notify one thread waiting for signal.
     pub fn notify_one(&self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_one(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_one(&self.counter);
+        }
     }
 
     /// Notify one thread waiting for all signal.
     pub fn notify_all(&self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_one(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_all(&self.counter);
+        }
     }
 
     /// Wait for notifying signal. May waking up spuriously.
     pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        // Protected by Mutex, so Relaxed is enough in correct use of CondVar.
+        self.num_waiters.fetch_add(1, Relaxed);
         let counter_value = self.counter.load(Relaxed);
 
         // Remember the mutex reference and release it.
@@ -38,6 +46,10 @@ impl Condvar {
 
         // Wait for notifying.
         wait(&self.counter, counter_value);
+
+        // No notifying is needed if spurious wake-up happens.
+        // It's safe to use relaxed ordering on here.
+        self.num_waiters.fetch_sub(1, Relaxed);
 
         // Lock the mutex after notifying.
         mutex.lock()
